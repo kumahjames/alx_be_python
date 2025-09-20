00f5b0f06438c7be501f2c94d66b4136f3648fc0
kumahjames <miraclearena7@gmail.com>
2025-09-20 19:06:07 +0100

    Fixed multiplication table formatting and syntax

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,4 +3,4 @@ number = int(input("Enter a number to see its multiplication table: "))
 print(f"\nMultiplication tabel for {number}: ")
 for i in range(1, 11):
     result = number * i
-    print(f"{number}, {i} = {result}")
+    print(f"{number} * {i} = {result}")
